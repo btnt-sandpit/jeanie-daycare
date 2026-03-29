eee8e1cfefe3b415632b1e095ff2572f5c6c1785
btnt-sandpit <bentrieu@gmail.com>
2026-03-29 23:16:06 +1300

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,3 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()]
 });
-```
-
-Press **Ctrl + S** to save.
-
-Then push the changes:
-```
-git add .
-```
-```
-git commit -m "remove vite-plugin-pwa"
-```
-```
-git push
